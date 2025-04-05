571b3f0c21c785507c2e1acd6538dd5476570f3b
Timo Strackfeldt <timo@strackfeldt.com>
2025-04-05 14:38:43 +0200

    chore: update lazyvim

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"folke/snacks.nvim",
+		opts = {
+			dashboard = { enabled = false },
+			scroll = { enabled = false },
+			indent = { enabled = false },
+			picker = {
+				sources = {
+					explorer = {
+						-- layout = { preset = "default", preview = true },
+						auto_close = true,
+					},
+				},
+			},
+		},
+	},
+}
--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,13 +1,13 @@
 return {
-  {
-    "supermaven-inc/supermaven-nvim",
-    lazy = false,
-    opts = {
-      keymaps = {
-        accept_suggestion = "<C-l>",
-        clear_suggestion = "<C-]>",
-        accept_word = "<C-j>",
-      },
-    },
-  },
+	{
+		"supermaven-inc/supermaven-nvim",
+		opts = {
+			keymaps = {
+				-- accept_suggestion = "<C-l>",
+				accept_suggestion = "<Tab>",
+				clear_suggestion = "<C-]>",
+				accept_word = "<C-j>",
+			},
+		},
+	},
 }
